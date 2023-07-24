aaab8552f4b36960a38c52c1b112d4ad175313a8
Andre Assunção <Decontas@hotmail.com>
2023-07-24 10:12:05 -0300

    att

--- script.js
+++ script.js
@@ -1,127 +1,174 @@
-let livros = {
-    data:[
-    {
-        nomeLivro:"Capão pecado",
+let products = {
+    data: [
+      {
+        productName: "Capão pecado",
         autor:"Ferrez",
-        categoria:"Romance",
-        preco:"28.50",
-        capa:"img/capas/capaopecado-capa.jpg",
-    },
-
-    {
-        nomeLivro:"Padrões JavaScript",
+        category: "Romance",
+        price: "30",
+        image: "/img/capas/capaopecado-capa.jpg",
+  
+      },
+      {
+        productName:"Padrões JavaScript",
         autor:"Jão Almeida",
-        categoria:"Tecnologia",
-        preco:"55.50",
-        capa:"img/capas/padroesjs.jpg",
-    },
-
-    {
-        nomeLivro:"Autobiografia Martin LKJ",
+        category: "Tecnologia",
+        price: "49",
+        image: "/img/capas/padroesjs.jpg",
+  
+      },
+      {
+        productName:"Autobiografia Martin LKJ",
         autor:"Martin Luther King Jr",
-        categoria:"Historia",
-        preco:"30.00",
-        capa:"img/capas/martinLT-biografia.jpg",
-    },
-
-    {
-        nomeLivro:"A coisa",
+        category: "Historia",
+        price: "99",
+        image: "/img/capas/martinLT-biografia.jpg",
+      },
+      {
+        productName:"A coisa",
         autor:"Stephen King",
-        categoria:"Terror",
-        preco:"22.50",
-        capa:"./img/capas/a-coisa.jpg",
-    },
-    {
-        nomeLivro:"A coisa",
-        autor:"Stephen King",
-        categoria:"Terror",
-        preco:"22.50",
-        capa:"./img/capas/a-coisa.jpg",
-    },
-
-    {
-        nomeLivro:"Autobiografia Martin LKJ",
-        autor:"Martin Luther King Jr",
-        categoria:"Historia",
-        preco:"30.00",
-        capa:"img/capas/martinLT-biografia.jpg",
-    },
-
-    
- ],
-
-};
-
-for (let i of livros.data) {
-    //criando cartao
+        category:"Terror",
+        price: "29",
+        image: "/img/capas/a-coisa.jpg",
+  
+        
+      },
+      {
+        productName: "Capão pecado",
+        autor:"Ferrez",
+        category: "Romance",
+        price: "30",
+        image: "/img/capas/capaopecado-capa.jpg",
+  
+      },
+      {
+        productName: "Capão pecado",
+        autor:"Ferrez",
+        category: "Romance",
+        price: "30",
+        image: "/img/capas/capaopecado-capa.jpg",
+  
+      },
+      {
+        productName: "Capão pecado",
+        autor:"Ferrez",
+        category: "Romance",
+        price: "30",
+        image: "/img/capas/capaopecado-capa.jpg",
+  
+      },
+      {
+        productName: "Capão pecado",
+        autor:"Ferrez",
+        category: "Romance",
+        price: "30",
+        image: "/img/capas/capaopecado-capa.jpg",
+  
+      },
+      {
+        productName: "Capão pecado",
+        autor:"Ferrez",
+        category: "Romance",
+        price: "30",
+        image: "/img/capas/capaopecado-capa.jpg",
+  
+      },
+      {
+        productName: "Capão pecado",
+        autor:"Ferrez",
+        category: "Romance",
+        price: "30",
+        image: "/img/capas/capaopecado-capa.jpg",
+  
+      },
+    ],
+  };
+  
+  for (let i of products.data) {
+    //Create Card
     let card = document.createElement("div");
-    //card tem que ter categoria e deve ficar inicialmente escondida
-    card.classList.add("card-livros", i.categoria, "hide");
+    //Card should have category and should stay hidden initially
+    card.classList.add("card-livros", i.category, "hide");
     //image div
     let imgContainer = document.createElement("div");
     imgContainer.classList.add("capa-container");
     //img tag
-    let capa = document.createElement("img");
-    capa.setAttribute("src", i.capa);
-    imgContainer.appendChild(capa)
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
     card.appendChild(imgContainer);
     //container
     let container = document.createElement("div");
     container.classList.add("container");
-    //nome livro
+    //product name
     let name = document.createElement("h5");
-    name.classList.add("nome-livro");
-    name.innerText = i.nomeLivro.toUpperCase();
-    container.appendChild(name)
-    //nome livro
-    let autor = document.createElement("h5");
-    autor.classList.add("autor-livro");
-    autor.innerText = i.autor.toUpperCase();
-    container.appendChild(autor)
+    name.classList.add("product-name");
+    name.innerText = i.productName.toUpperCase();
+    container.appendChild(name);
     
     
-
+    //price
+    let price = document.createElement("h6");
+    price.innerText = "$" + i.price;
+    container.appendChild(price);
+  
     card.appendChild(container);
-
-    document.getElementById("livros").appendChild(card);
-}
-
-//parâmetro passado do botão (parâmetro igual à categoria)
-function filterProduct(value){
-    //button de classe
+    document.getElementById("products").appendChild(card);
+  }
+  
+  //parameter passed from button (Parameter same as category)
+  function filterProduct(value) {
+    //Button class code
     let buttons = document.querySelectorAll(".button-value");
     buttons.forEach((button) => {
-        //check os innerText
-        if(value.toUpperCase() == button.innerText.toUpperCase()){
-            button.classList.add("active");
-        } else{
-            button.classList.remove("active");
-        }
-
-        
+      //check if value equals innerText
+      if (value.toUpperCase() == button.innerText.toUpperCase()) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
     });
-
+  
     //select all cards
     let elements = document.querySelectorAll(".card-livros");
     //loop through all cards
     elements.forEach((element) => {
-        //display all cards on "all" button click
-        if (value == "todos") {
-            element.classList.remove("hide");
+      //display all cards on 'all' button click
+      if (value == "todos") {
+        element.classList.remove("hide");
+      } else {
+        //Check if element contains category class
+        if (element.classList.contains(value)) {
+          //display element based on category
+          element.classList.remove("hide");
+        } else {
+          //hide other elements
+          element.classList.add("hide");
         }
-        else{
-            //check if element contains category class
-            if(element.classList.contains(value)){
-               //display baseado na categoria 
-               element.classList.remove("hide");   
-            }  else {
-                //escondendo outros elementos 
-                element.classList.add("hide");
-            }
-        }
+      }
     });
-}
-
+  }
+  
+  //Search button click
+  document.getElementById("search").addEventListener("click", () => {
+    //initializations
+    let searchInput = document.getElementById("search-input").value;
+    let elements = document.querySelectorAll(".product-name");
+    let cards = document.querySelectorAll(".card-livros");
+  
+    //loop through all elements
+    elements.forEach((element, index) => {
+      //check if text includes the search value
+      if (element.innerText.includes(searchInput.toUpperCase())) {
+        //display matching card
+        cards[index].classList.remove("hide");
+      } else {
+        //hide others
+        cards[index].classList.add("hide");
+      }
+    });
+  });
+  
+ 
 //Mostrar inicialmente todos os produtos
 window.onload = () => {
     filterProduct("todos");
