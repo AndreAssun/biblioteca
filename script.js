10a952d7f5fa684f25579a0699195ce3b8a0e8a9
Andre Assunção <Decontas@hotmail.com>
2023-07-25 17:22:31 -0300

    atualizações acervo

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 let products = {
     data: [
       {
+        productName: "Malcom X",
+        autor:"Luiz Carlos Santos",
+        category: "Historia",
+        price: "30",
+        image: "/img/capas/MALCONX.jpg",
+  
+      },
+      
+      {
         productName: "Capão pecado",
         autor:"Ferrez",
         category: "Romance",
@@ -17,35 +26,35 @@ let products = {
   
       },
       {
-        productName:"Padrões JavaScript",
-        autor:"Jão Almeida",
+        productName:"Introdução á linguagem SQL",
+        autor:"Thomas Nield",
         category: "Tecnologia",
-        price: "49",
-        image: "/img/capas/padroesjs.jpg",
+        price: "55",
+        image: "/img/capas/sql.jpg",
   
       },
       {
-        productName:"Padrões JavaScript",
-        autor:"Jão Almeida",
+        productName:"Banco de dados",
+        autor:"Willian Pereira Alves",
         category: "Tecnologia",
         price: "49",
-        image: "/img/capas/padroesjs.jpg",
+        image: "/img/capas/bancodados.jpg",
   
       },
       {
-        productName:"Padrões JavaScript",
-        autor:"Jão Almeida",
+        productName:"Aprendendo Node",
+        autor:"Shelley Powers",
         category: "Tecnologia",
         price: "49",
-        image: "/img/capas/padroesjs.jpg",
+        image: "/img/capas/node.jpg",
   
       },
       {
-        productName:"Padrões JavaScript",
-        autor:"Jão Almeida",
+        productName:"Introdução á programação Python",
+        autor:"Nilo Ney",
         category: "Tecnologia",
         price: "49",
-        image: "/img/capas/padroesjs.jpg",
+        image: "/img/capas/phyton.jpg",
   
       },
       {
@@ -65,61 +74,76 @@ let products = {
         
       },
       {
-        productName: "Capão pecado",
-        autor:"Ferrez",
+        productName: "Dono do Morro",
+        autor:"Caco Barcellos",
         category: "Romance",
         price: "30",
-        image: "/img/capas/capaopecado-capa.jpg",
+        image: "/img/capas/donodomorro.jpg",
   
       },
       {
-        productName: "Capão pecado",
-        autor:"Ferrez",
+        productName: "Romeu e Julieta",
+        autor:"William Shakespeare",
         category: "Romance",
         price: "30",
-        image: "/img/capas/capaopecado-capa.jpg",
+        image: "/img/capas/romeu-julieta.jpg",
   
       },
       {
-        productName: "Capão pecado",
-        autor:"Ferrez",
+        productName: "A culpa é das estrelas",
+        autor:"Harry Jon",
         category: "Romance",
         price: "30",
-        image: "/img/capas/capaopecado-capa.jpg",
+        image: "/img/capas/culpadasestrelas.jpg",
   
       },
       {
-        productName: "Capão pecado",
-        autor:"Ferrez",
-        category: "Romance",
+        productName: "O iluminado",
+        autor:"Stephen King",
+        category: "Terror",
         price: "30",
-        image: "/img/capas/capaopecado-capa.jpg",
+        image: "/img/capas/o-iluminado.jpg",
   
       },
       {
-        productName: "Capão pecado",
-        autor:"Ferrez",
-        category: "Romance",
+        productName: "The Amityville Horror",
+        autor:"Stephen King",
+        category: "Terror",
         price: "30",
-        image: "/img/capas/capaopecado-capa.jpg",
+        image: "/img/capas/amityville.jpg",
   
       },
       {
-        productName: "Capão pecado",
-        autor:"Ferrez",
-        category: "Romance",
+        productName: "Cemitério maldito",
+        autor:"Stephen King",
+        category: "Terror",
         price: "30",
-        image: "/img/capas/capaopecado-capa.jpg",
+        image: "/img/capas/cemiterio.jpg",
   
       },
+      {
+        productName: "Luiz Gama",
+        autor:"Luiz Carlos Santos",
+        category: "Historia",
+        price: "30",
+        image: "/img/capas/luizgama.jpg",
+  
+      },
+      
     ],
   };
   
-  for (let i of products.data) {
+ for (let i of products.data) {
     //Create Card
     let card = document.createElement("div");
+    
     //Card should have category and should stay hidden initially
     card.classList.add("card-livros", i.category, "hide");
+    
+    // Create an anchor element
+    let anchor = document.createElement("a");
+    anchor.setAttribute("href", i.link); // Set the href attribute to the link from the data
+
     //image div
     let imgContainer = document.createElement("div");
     imgContainer.classList.add("capa-container");
@@ -127,25 +151,31 @@ let products = {
     let image = document.createElement("img");
     image.setAttribute("src", i.image);
     imgContainer.appendChild(image);
-    card.appendChild(imgContainer);
+    anchor.appendChild(imgContainer);
+
     //container
     let container = document.createElement("div");
     container.classList.add("container");
     //product name
     let name = document.createElement("h5");
     name.classList.add("product-name");
-    name.innerText = i.productName.toUpperCase();
+    name.innerText = i.productName;
     container.appendChild(name);
+
+    //autor name
+    let autor = document.createElement("h3");
+    autor.classList.add("autor");
+    autor.innerText = i.autor;
+    container.appendChild(autor);
     
+    // Append the container to the anchor instead of the card
+    anchor.appendChild(container);
     
-    //price
-    let price = document.createElement("h6");
-    price.innerText = "$" + i.price;
-    container.appendChild(price);
-  
-    card.appendChild(container);
+    // Append the anchor to the card
+    card.appendChild(anchor);
+    
     document.getElementById("products").appendChild(card);
-  }
+}
   
   //parameter passed from button (Parameter same as category)
   function filterProduct(value) {
@@ -180,25 +210,25 @@ let products = {
     });
   }
   
-  //Search button click
-  document.getElementById("search").addEventListener("click", () => {
-    //initializations
-    let searchInput = document.getElementById("search-input").value;
-    let elements = document.querySelectorAll(".product-name");
-    let cards = document.querySelectorAll(".card-livros");
-  
-    //loop through all elements
-    elements.forEach((element, index) => {
-      //check if text includes the search value
-      if (element.innerText.includes(searchInput.toUpperCase())) {
-        //display matching card
-        cards[index].classList.remove("hide");
-      } else {
-        //hide others
-        cards[index].classList.add("hide");
-      }
-    });
+  // Search button click
+document.getElementById("search").addEventListener("click", () => {
+  // Initialization
+  let searchInput = document.getElementById("search-input").value.toLowerCase();
+  let elements = document.querySelectorAll(".product-name");
+  let cards = document.querySelectorAll(".card-livros");
+
+  // Loop through all elements
+  elements.forEach((element, index) => {
+    // Check if text includes the search value (case-insensitive and partial match)
+    if (element.innerText.toLowerCase().includes(searchInput)) {
+      // Display matching card
+      cards[index].classList.remove("hide");
+    } else {
+      // Hide others
+      cards[index].classList.add("hide");
+    }
   });
+});
   
  
 //Mostrar inicialmente todos os produtos
